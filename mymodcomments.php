7a0c3cc2a1c3b9e25425f4cddb46b647f8e35dc9
Apostol123 <apostol516@gmail.com>
2019-05-31 07:56:53 +0200

    implements the function in wich you can add the coments to th db and rettrive it back and show it to the user the comments of the specif prodcut and implements the configuration of the module to be applyed corectly

--- mymodcomments.php
+++ mymodcomments.php
@@ -35,28 +35,71 @@ class mymodcomments extends Module
             $enable_comments = Tools::getValue('enable_comments');
             Configuration::updateValue('MYMOD_GRADES', $enable_grades);
             Configuration::updateValue('MYMOD_COMMENTS', $enable_comments);
-            $this->context->smarty->assign("confirmation","ok");
+            $this->context->smarty->assign("confirmation", "ok");
         }
     }
 
-    public function assignConfiguration(){
+    public function assignConfiguration()
+    {
         $enable_grades = Configuration::get('MYMOD_GRADES');
         $enable_comments = Configuration::get('MYMOD_COMMENTS');
         $this->context->smarty->assign('enable_grades', $enable_grades);
-        $this->context->smarty->assign('enable_comments',
-        $enable_comments);
+        $this->context->smarty->assign(
+            'enable_comments',
+            $enable_comments
+        );
     }
 
-        //Allows us to add the mod to a certain point(hook) in the font page
-        // in this case in the display product tab
-    public function install(){
+    //Allows us to add the mod to a certain point(hook) in the font page
+    // in this case in the display product tab
+    public function install()
+    {
         parent::install();
         $this->registerHook("displayProductTabContent");
         return true;
     }
 
     // dispalys the html into the hook
-    public function hookDisplayProductTabContent($params){
-        return "<b> Display me on the product page </b>";
+    public function hookDisplayProductTabContent($params)
+    {
+        $this->processProductTabContent();
+        $this->assignProductTabContent();
+        return $this->display(__FILE__, "displayProductTabContent.tpl");
+    }
+
+    // method that retirves the data from the form in the hook
+    //and saves the data in the DB in our module table
+    public function processProductTabContent()
+    {
+        if (Tools::isSubmit("mymod_pc_submit_comment")) {
+            $id_product = Tools::getValue('id_product');
+            $grade = Tools::getValue('grade');
+            $comment = Tools::getValue('comment');
+
+            $insert = array(
+                "id_product" => (int)$id_product,
+                "grade" => (int)$grade,
+                "comment" => pSQL($comment),
+                "date_add" => date("Y-m-d H:i:s")
+            );
+
+            Db::getInstance()->insert("mymod_comment", $insert);
+        }
+    }
+
+    public function assignProductTabContent()
+    {
+        $enable_grades = Configuration::get('MYMOD_GRADES');
+        $enable_comments = Configuration::get('MYMOD_COMMENTS');
+        $id_product = Tools::getValue('id_product');
+        $comments = Db::getInstance()->executeS('SELECT * FROM
+        ' . _DB_PREFIX_ . 'mymod_comment WHERE id_product =
+        ' . (int)$id_product);
+        $this->context->smarty->assign('enable_grades', $enable_grades);
+        $this->context->smarty->assign(
+            'enable_comments',
+            $enable_comments
+        );
+        $this->context->smarty->assign('comments', $comments);
     }
 }
